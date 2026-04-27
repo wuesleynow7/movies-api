61cb28d187b525f2567b57bfb47c7af88fe01c0d
wuesley <wuesleynow7@gmail.com>
2026-04-27 17:05:04 -0300

    primeiras configuraçõesdo projeto

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import CourseList from './components/FilmList/FilmList.tsx'
+
+
+
+function App() {
+  return (
+    <> {/* <--- Adicionado o Fragment aqui */}
+      <CourseList />
+    </>
+  );
+}
+
+export default App
---
+++ src/components/FilmList/FilmList.tsx
@@ -0,0 +1,17 @@
+
+
+export default function CourseList(){
+
+
+    return (
+
+    <div>
+        <h1>pagina da lista de cursos</h1>
+
+        <div className="flex flex-wrap gap-4 justify-center md:justify-start">
+    
+        </div>
+    </div>
+
+    );
+}
